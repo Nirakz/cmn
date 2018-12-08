ca5a32df41e4856102a95cc8dc7c39e933d05b2e
trungquan17 <quanlightning17@gmail.com>
2018-12-09 00:13:55 +0700

    [1.0.0] Add BabelJS.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require("express");
-var app = express();
+import express from "express";
+let app = express();
 
-var hostname = "localhost";
-var port = 8017;
+let hostname = "localhost";
+let port = 8017;
 
 app.get("/helloworld", (req, res) => {
-    res.send("<h1>Hello world !!!!</h1>");
+  res.send("<h1>Hello world !!!!</h1>");
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Hello Trung Quan, I'm running at ${hostname}:${port}/`);
+  console.log(`Hello Trung Quan, I'm running at ${hostname}:${port}/`);
 });
