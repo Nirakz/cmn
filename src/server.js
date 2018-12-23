bb0543fa625a7b2e7500d57b4c6a6b75f02bbd53
Trung Quân <quanlightning17@gmail.com>
2018-12-24 06:02:14 +0700

    Revert "[1.0.0] Connect & create new item MongoDB."

--- src/server.js
+++ src/server.js
@@ -1,25 +1,13 @@
 import express from "express";
-import ConnectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model";
-
 let app = express();
 
-// Connect to MongoDB
-ConnectDB();
+let hostname = "localhost";
+let port = 8017;
 
-app.get("/test-database", async (req, res) => {
-  try {
-    let item = {
-      userId: "17071995",
-      contactId: "1707199517071995",
-    };
-    let contact = await ContactModel.createNew(item);
-    res.send(contact);
-  } catch (err) {
-    console.log(err);
-  }
+app.get("/helloworld", (req, res) => {
+  res.send("<h1>Hello world !!!!</h1>");
 });
 
-app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
-  console.log(`Hello Trung Quan, I'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`);
+app.listen(port, hostname, () => {
+  console.log(`Hello Trung Quan, I'm running at ${hostname}:${port}/`);
 });
