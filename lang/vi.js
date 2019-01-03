30f4102ae8abc2b22df378a17779284ebe93d73b
trungquan17 <quanlightning17@gmail.com>
2019-01-03 21:39:57 +0700

    [1.0.0] Register user function part 01.

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const validation = {
+  "email_incorrect": "Email phải có dạng example@trungquandev.com !",
+  "gender_incorrect": "Ủa, tại sao trường giới tính lại sai?",
+  "password_incorrect": "Mật khẩu phải chứa ít nhất 8 ký tự bao gồm chữ hoa, chữ thường, chữ số và ký tự đặc biệt.",
+  "password_confirmation_incorrect": "Nhập lại mật khẩu chưa chính xác!"
+};
