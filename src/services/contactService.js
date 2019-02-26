9131e4eaee1aee2cb792f2ce15c983ac4a5dd859
trungquan17 <quanlightning17@gmail.com>
2019-02-26 21:44:54 +0700

    [1.0.0] Render notifications to view.

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,5 +1,6 @@
 import ContactModel from "./../models/contactModel";
 import UserModel from "./../models/userModel";
+import NotificationModel from "./../models/notificationModel";
 import _ from "lodash";
 
 let findUsersContact = (currentUserId, keyword) => {
@@ -23,12 +24,21 @@ let addNew = (currentUserId, contactId) => {
     if (contactExists) {
       return reject(false);
     }
-
+    // create contact
     let newContactItem = {
       userId: currentUserId,
       contactId: contactId
     };
     let newContact = await ContactModel.createNew(newContactItem);
+
+    // create notification
+    let notificationItem = {
+      senderId: currentUserId,
+      receiverId: contactId,
+      type: NotificationModel.types.ADD_CONTACT,
+    };
+    await NotificationModel.model.createNew(notificationItem);
+
     resolve(newContact);
   });
 }
@@ -39,6 +49,9 @@ let removeRequestContact = (currentUserId, contactId) => {
     if (removeReq.result.n === 0) {
       return reject(false);
     }
+    // remove notification
+    let notifTypeAddContact = NotificationModel.types.ADD_CONTACT;
+    await NotificationModel.model.removeRequestContactNotification(currentUserId, contactId, notifTypeAddContact);
     resolve(true);
   });
 }
