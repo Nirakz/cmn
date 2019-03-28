8326c2dba878253590222d7d9ab750fbce2a148a
trungquan17 <quanlightning17@gmail.com>
2019-03-28 22:41:55 +0700

    [1.0.0] Render messages to view.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -42,8 +42,8 @@ let updateAvatar = (req, res) => {
       // Update user
       let userUpdate = await user.updateUser(req.user._id, updateUserItem);
 
-      // Remove old user avatar
-      await fsExtra.remove(`${app.avatar_directory}/${userUpdate.avatar}`);
+      // Không xóa avatar cũ của người dùng, vì trong bảng messages cần để sử dụng
+      // await fsExtra.remove(`${app.avatar_directory}/${userUpdate.avatar}`);
 
       let result = {
         message: transSuccess.user_info_updated,
