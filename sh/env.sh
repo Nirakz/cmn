78f9dff23b0cadf8b3a3e98a967de12edeb2b7df
nirakz <ntchinh271099@gmail.com>
2020-11-17 16:20:37 +0700

    second deploy

---
+++ sh/env.sh
@@ -0,0 +1,31 @@
+# # Config database environtment variable
+# export DB_CONNECTION=mongodb
+# export DB_HOST=localhost
+# export DB_PORT=27017
+# export DB_NAME=awesome_chat
+# export DB_USERNAME=""
+# export DB_PASSWORD=""
+
+# Config app environtment variable
+export APP_HOST=localhost
+export APP_PORT=1227
+
+# Config session key & secret
+export SESSION_KEY="thanhchinh"
+export SESSION_SECRET="greencat"
+
+# Config admin email account
+export MAIL_USER=alochat2.0.0@gmail.com
+export MAIL_PASSWORD=0362944566chinh
+export MAIL_HOST=smtp.gmail.com
+export MAIL_PORT=587
+
+# Config facebook login app
+export FB_APP_ID=test
+export FB_APP_SECRET=test
+export FB_CALLBACK_URL=test
+
+# Config google login app
+export GG_APP_ID=test
+export GG_APP_SECRET=test
+export GG_CALLBACK_URL=test
