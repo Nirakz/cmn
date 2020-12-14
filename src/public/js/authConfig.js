27b7e9e4f889b6ed141549059ea59fb77d394b5d
Nirakz <ntchinh271099@gmail.com>
2020-12-14 14:07:08 +0700

    da them duoc

--- src/public/js/authConfig.js
+++ src/public/js/authConfig.js
@@ -1,40 +1,46 @@
-/**
- * Created by https://trungquandev.com's author on 25/02/2018.
- */
 function showRegisterForm() {
-    $('.loginBox').fadeOut('fast', function() {
-      $('.registerBox').fadeIn('fast');
-      $('.login-footer').fadeOut('fast', function() {
-        $('.register-footer').fadeIn('fast');
-      });
-      $('.modal-title').html('Đăng ký tài khoản');
+  $('.loginBox').fadeOut('fast', function() {
+    $('.registerBox').fadeIn('fast');
+    $('.login-footer').fadeOut('fast', function() {
+      $('.register-footer').fadeIn('fast');
     });
-    $('.error').removeClass('alert alert-danger').html('');
-  
-  }
-  
-  function showLoginForm() {
-    $('#loginModal .registerBox').fadeOut('fast', function() {
-      $('.loginBox').fadeIn('fast');
-      $('.register-footer').fadeOut('fast', function() {
-        $('.login-footer').fadeIn('fast');
-      });
-  
-      $('.modal-title').html('Đăng nhập');
-    });
-    $('.error').removeClass('alert alert-danger').html('');
-  }
-  
-  function openLoginModal() {
-    setTimeout(function() {
-      $('#loginModal').modal('show');
-      showLoginForm();
-    }, 230);
-  }
-  
-  function openRegisterModal() {
-    setTimeout(function() {
-      $('#loginModal').modal('show');
-      showRegisterForm();
-    }, 230);
-  }
+    $('.modal-title').html('Đăng ký tài khoản');
+  });
+  $('.error').removeClass('alert alert-danger').html('');
+
+}
+function showAddForm() {
+  $('#loginModal .registerBox').fadeOut('fast', function() {
+    $('.loginBox').fadeIn('fast');
+    $('.modal-title').html('Add User');
+  });
+  $('.error').removeClass('alert alert-danger').html('');
+}
+
+function showLoginForm() {
+  $('#AddModal').fadeOut('fast', function() {
+    $('.addBox').fadeIn('fast');
+  });
+  $('.error').removeClass('alert alert-danger').html('');
+}
+
+function openLoginModal() {
+  setTimeout(function() {
+    $('#loginModal').modal('show');
+    showLoginForm();
+  }, 230);
+}
+
+function openRegisterModal() {
+  setTimeout(function() {
+    $('#loginModal').modal('show');
+    showRegisterForm();
+  }, 230);
+}
+function openAddModal() {
+  setTimeout(function() {
+    $('#AddModal').modal('show');
+    showAddForm();
+  }, 230);
+}
+
