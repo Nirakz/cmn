81e04dd4143444d4540fdf8750d28d19a7445c86
Nirakz <ntchinh271099@gmail.com>
2020-12-18 16:38:01 +0700

    sh

--- sh/env.sh
+++ sh/env.sh
@@ -1,13 +1,6 @@
-# Config database environtment variable
-export DB_CONNECTION=mongodb
-export DB_HOST=localhost
-export DB_PORT=27017
-export DB_NAME=alochat-123
-export DB_USERNAME=""
-export DB_PASSWORD=""
 
 # Config app environtment variable
-export APP_HOST=localhost
+export APP_HOST=ec2-54-255-68-67.ap-southeast-1.compute.amazonaws.com
 export APP_PORT=1227
 
 # Config session key & secret
@@ -21,9 +14,9 @@ export MAIL_HOST=smtp.gmail.com
 export MAIL_PORT=587
 
 # Config facebook login app
-export FB_APP_ID=test
-export FB_APP_SECRET=test
-export FB_CALLBACK_URL=test
+export FB_APP_ID=728133371130609
+export FB_APP_SECRET=7ed066da498a2e2b161630581d4f1d9d
+export FB_CALLBACK_URL=http://ec2-54-255-68-67.ap-southeast-1.compute.amazonaws.com:1227/auth/facebook/callback
 
 # Config google login app
 export GG_APP_ID=test
