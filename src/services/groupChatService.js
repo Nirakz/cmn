39c25046c6423e8eb8d36d9e2bfaa64d265bba9a
nirakz <ntchinh271099@gmail.com>
2022-04-12 16:52:57 +0700

    fix2.0

---
+++ src/services/groupChatService.js
@@ -0,0 +1,39 @@
+import _ from "lodash";
+import ChatGroupModel from "./../models/chatGroupModel";
+import UserModel from "./../models/userModel";
+
+let addNewGroup = (currentUserId, arrayMemberIds, groupChatName) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // add current userId to array members
+      arrayMemberIds.unshift({userId: `${currentUserId}`});
+      
+      arrayMemberIds = _.uniqBy(arrayMemberIds, "userId");
+
+      let newGroupItem = {
+        name: groupChatName,
+        userAmount: arrayMemberIds.length,
+        userId: `${currentUserId}`,
+        members: arrayMemberIds
+      };
+
+      let newGroup = await ChatGroupModel.createNew(newGroupItem);
+
+      // extras get userInfo
+      newGroup = newGroup.toObject();
+      newGroup.membersInfo = [];
+      for (let member of newGroup.members) {
+        let userInfo = await UserModel.getNormalUserDataById(member.userId);
+        newGroup.membersInfo.push(userInfo);
+      }
+      
+      resolve(newGroup);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  addNewGroup: addNewGroup
+};
