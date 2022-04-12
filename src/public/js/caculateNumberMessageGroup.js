250ecf73a10363fc2c14a8c229804e165377326f
nirakz <ntchinh271099@gmail.com>
2022-04-12 16:48:28 +0700

    fix2.0

--- src/public/js/caculateNumberMessageGroup.js
+++
@@ -1,6 +0,0 @@
-function increaseNumberMessageGroup(divId) {
-  let currentValue = +$(`.right[data-chat=${divId}]`).find("span.show-number-messages").text();
-  currentValue += 1;
-
-  $(`.right[data-chat=${divId}]`).find("span.show-number-messages").html(currentValue);
-}
