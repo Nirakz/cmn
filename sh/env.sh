fc452d9dd9f2db4ac106155fa21269d31445965f
nirakz <ntchinh271099@gmail.com>
2022-05-11 12:04:33 +0700

    Deploy to Heroku

--- sh/env.sh
+++ sh/env.sh
@@ -1,22 +1,22 @@
 
 # Config app environtment variable
-export APP_HOST=ec2-54-255-68-67.ap-southeast-1.compute.amazonaws.com
-export APP_PORT=1227
+export APP_HOST='https://alo-chat-dori.herokuapp.com'
+export APP_PORT=3020
 
 # Config session key & secret
 export SESSION_KEY="thanhchinh"
 export SESSION_SECRET="greencat"
 
 # Config admin email account
-export MAIL_USER=alochatcnm@gmail.com
-export MAIL_PASSWORD=0362944566chinh
+export MAIL_USER=ntchinh271019993@gmail.com
+export MAIL_PASSWORD=00362944566
 export MAIL_HOST=smtp.gmail.com
 export MAIL_PORT=587
 
 # Config facebook login app
-export FB_APP_ID=728133371130609
-export FB_APP_SECRET=7ed066da498a2e2b161630581d4f1d9d
-export FB_CALLBACK_URL=http://ec2-54-255-68-67.ap-southeast-1.compute.amazonaws.com:1227/auth/facebook/callback
+export FB_APP_ID=test
+export FB_APP_SECRET=test
+export FB_CALLBACK_URL=test
 
 # Config google login app
 export GG_APP_ID=test
